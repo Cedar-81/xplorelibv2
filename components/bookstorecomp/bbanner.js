9380976475e50ec2291596a339f93d02d72c0ecb
Cedar-81 <64336732+Cedar-81@users.noreply.github.com>
2022-05-05 12:38:05 -1100

    added react-icons to package.json

--- components/bookstorecomp/bbanner.js
+++ components/bookstorecomp/bbanner.js
@@ -3,7 +3,7 @@ import booksLib from '../../pages/api/booksLib'
 import genre from '../../pages/api/genre'
 
 import Card from '../xplorecomp/card'
-import * as MdIcons from 'react-icons'
+import * as MdIcons from 'react-icons/md'
 import { useState, useEffect } from 'react'
 
 
